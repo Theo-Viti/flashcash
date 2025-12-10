7cd2e201cb5bd8f3da9fb7b6e9f30a7a02708e12
Théo Viti <theo.viti2@outlook.fr>
2025-12-10 10:57:23 +0100

    feat:spring-security-config added

--- src/main/java/com/example/flashcash/repository/UserRepository.java
+++ src/main/java/com/example/flashcash/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.example.flashcash.repository;
 import com.example.flashcash.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+      Optional<User> findUserByMail (String email); //Optional avoid NullPointerException if null
+
 }
--- src/main/java/com/example/flashcash/service/UserAccountService.java
+++ src/main/java/com/example/flashcash/service/UserAccountService.java
@@ -18,7 +18,7 @@ public class UserAccountService {
 
       //Methods
       public UserAccount deposit(Integer accountId, double amount){
-            deposit(accountId, amount);
+
 
       }
 
