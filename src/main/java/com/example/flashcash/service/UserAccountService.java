85e00ffaf049841fe79a4fe80bd3dda2c8ea24f4
Théo Viti <theo.viti2@outlook.fr>
2025-12-10 16:45:06 +0100

    feat: UserAccount balance logic added #4

--- src/main/java/com/example/flashcash/service/UserAccountService.java
+++ src/main/java/com/example/flashcash/service/UserAccountService.java
@@ -1,6 +1,7 @@
 package com.example.flashcash.service;
 
 
+import com.example.flashcash.model.User;
 import com.example.flashcash.model.UserAccount;
 import com.example.flashcash.repository.UserAccountRepository;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,25 @@ public class UserAccountService {
       //Methods
       public UserAccount deposit(Integer accountId, double amount){
 
+            if (amount <= 0) { //Positive deposit amount check
+                  throw new IllegalArgumentException("Deposit amount must be positive");
+            }
+            UserAccount account = userAccountRepository.findById(accountId) //Account check
+                    .orElseThrow(() -> new RuntimeException("Account not found"));
+
+            account.plus(amount);
+
+            return userAccountRepository.save(account);
+            }
+
 
       }
 
-}
+//      public UserAccount withdraw(Integer accountId, double amount) {
+//
+//      }
+//
+//      public void transfer(Integer fromAccountId, Integer toAccountId, double amount){
+//
+//      }
+
