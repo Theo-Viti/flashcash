37d9fbbf6d6a35d7a2ad7684712c54249cd5745a
Théo Viti <theo.viti2@outlook.fr>
2025-12-11 16:44:36 +0100

    feat: register and login logic added

--- src/main/java/com/example/flashcash/service/UserService.java
+++ src/main/java/com/example/flashcash/service/UserService.java
@@ -1,8 +1,50 @@
 package com.example.flashcash.service;
 
 
+import com.example.flashcash.model.User;
+import com.example.flashcash.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-}
+
+
+      private final UserRepository userRepository;
+      private final PasswordEncoder passwordEncoder;
+
+      public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
+            this.userRepository = userRepository;
+            this.passwordEncoder = passwordEncoder;
+
+      }
+
+
+
+
+      public User register(String firstName, String lastName, String email, String password){
+
+            String hashedPassword = passwordEncoder.encode(password);
+
+            User user = new User();
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+            user.setEmail(email);
+            user.setPassword(hashedPassword);
+
+            return userRepository.save(user);
+      }
+
+      public User login(String email, String password) {
+
+                  User user = userRepository.findUserByMail(email)
+                          .orElseThrow(() -> new IllegalArgumentException("Wrong credentials"));
+
+                  if (!passwordEncoder.matches(password, user.getPassword())) {
+                        throw new IllegalArgumentException("Wrong credentials");
+                  }
+                  return user;
+            }
+      }
+
+
